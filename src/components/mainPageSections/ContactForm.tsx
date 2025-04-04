ab0185ad775874df54e3f144b169a8f65888c307
antonis <antwnis_skarlatos@yahoo.com>
2025-04-04 17:16:16 +0300

    small fix

--- src/components/mainPageSections/ContactForm.tsx
+++ src/components/mainPageSections/ContactForm.tsx
@@ -35,7 +35,7 @@ export default function ContactForm() {
     setErrors(null);
     try {
       const res = await fetch(
-        `https://formsubmit.co/ajax/${process.env.FIGATA_EMAIL}`,
+        `https://formsubmit.co/ajax/${process.env.NEXT_PUBLIC_FIGATA_EMAIL}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
