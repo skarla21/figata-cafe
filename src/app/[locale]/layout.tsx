04e8803f9a81babdc10dfc327c7b2dc636819b4b
antonis <antwnis_skarlatos@yahoo.com>
2025-05-30 15:24:12 +0300

    Add robots.txt and sitemap.xml for SEO optimization; update layout metadata with improved descriptions and localization support.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,9 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Figata Café",
-  metadataBase: new URL("https://figata-cafe.vercel.app"), //change this
+  description:
+    "A cozy café in Vironas, Athens, offering specialty coffee, healthy snacks, and a warm atmosphere. Visit us for a unique coffee experience.",
+  metadataBase: new URL("https://figata.gr"),
   icons: {
     icon: [
       { url: "/assets/imgs/icon.ico", type: "image/png", sizes: "192x192" },
@@ -15,14 +17,32 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     title: "Figata Café",
+    description:
+      "A cozy café in Vironas, Athens, offering specialty coffee, healthy snacks, and a warm atmosphere. Visit us for a unique coffee experience.",
     siteName: "Figata Café",
+    locale: "el_GR",
     images: [
       {
         url: "/assets/imgs/logo.jpg",
         width: 720,
         height: 720,
+        alt: "Figata Café Logo",
       },
     ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Figata Café",
+    description:
+      "A cozy café in Vironas, Athens, offering specialty coffee, healthy snacks, and a warm atmosphere.",
+    images: ["/assets/imgs/logo.jpg"],
+  },
+  alternates: {
+    canonical: "https://figata.gr",
+    languages: {
+      en: "https://figata.gr/en",
+      el: "https://figata.gr/el",
+    },
   },
 };
 
